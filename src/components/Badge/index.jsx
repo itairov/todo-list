36483c41cbbb5d167c5836b3a9cad3e0fc97e6a6
Ibragim <agada2013@yandex.ru>
2021-02-21 23:06:58 +0300

    add

---
+++ src/components/Badge/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import classNames from 'classnames'
+import './Badge.scss'
+
+
+const Badge = ({ color, onClick, className }) => (
+  <i
+    onClick={onClick}
+    className={classNames('badge', {[`badge--${color}`]: color}, className)}
+  />
+);
+
+export default Badge;
+
+//1:56 (2video)
